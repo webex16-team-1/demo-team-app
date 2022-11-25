ec240a852166b50e8d67b6d3a92cb5b62732607f
Squash-h <h.squash.5103@icloud.com>
2022-11-25 17:24:44 +0900

    “とりあえずフッターだけ”

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div>
       This Application is made by
-      <span class="names">○○○, ○○○ and ○○○</span>.
+      <span class="names">win, dukky, takaba, and yuki</span>.
     </div>
     <small
       >The Application is created as a part of the GeekSalon WebExpert
